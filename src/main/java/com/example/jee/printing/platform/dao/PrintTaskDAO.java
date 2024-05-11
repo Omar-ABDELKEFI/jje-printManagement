a36a67c3d9a819ddb1d555e6391d0033b773f8e6
Mohamed Ali Abdelmalek <abdelmalek.medali@gmail.com>
2024-05-11 17:47:07 +0000

    fix agent print

---
+++ src/main/java/com/example/jee/printing/platform/dao/PrintTaskDAO.java
@@ -0,0 +1,45 @@
+package com.example.jee.printing.platform.dao;
+
+import com.example.jee.printing.platform.model.Task;
+import com.example.jee.printing.platform.utils.ConnectionUtils;
+
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintTaskDAO {
+    public List<Task> getAgentTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionUtils.getConnection();
+            // Changez la requête SQL en fonction de votre schéma de base de données
+            String query = "SELECT pr.user_id, u.email AS teacher_name, pr.numStudents AS num_copies, pr.arrivalDateTime AS reception_date, pr.document FROM PrintRequest pr JOIN User u ON pr.user_id = u.id";
+            System.out.println(query + " - query--------------");
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String teacherName = resultSet.getString("teacher_name");
+                int numCopies = resultSet.getInt("num_copies");
+                String receptionDate = resultSet.getString("reception_date");
+                String document = resultSet.getString("document");
+
+                Task task = new Task(teacherName, numCopies, receptionDate, document);
+                tasks.add(task);
+            }
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+        return tasks;
+    }
+}
---
+++ src/main/java/com/example/jee/printing/platform/model/Task.java
@@ -0,0 +1,47 @@
+package com.example.jee.printing.platform.model;
+
+public class Task {
+    private String teacherName;
+    private int numCopies;
+    private String receptionDate;
+    private String document;
+
+    public Task(String teacherName, int numCopies, String receptionDate, String document) {
+        this.teacherName = teacherName;
+        this.numCopies = numCopies;
+        this.receptionDate = receptionDate;
+        this.document = document;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public int getNumCopies() {
+        return numCopies;
+    }
+
+    public void setNumCopies(int numCopies) {
+        this.numCopies = numCopies;
+    }
+
+    public String getReceptionDate() {
+        return receptionDate;
+    }
+
+    public void setReceptionDate(String receptionDate) {
+        this.receptionDate = receptionDate;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+}
