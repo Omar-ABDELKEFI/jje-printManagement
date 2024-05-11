cbba3b5ad055ec73d85d0319147ca5339741ec47
Mohamed Ali Abdelmalek <abdelmalek.medali@gmail.com>
2024-05-11 22:22:54 +0000

    agent fixing

--- src/main/java/com/example/jee/printing/platform/controller/AgentDashboardServlet.java
+++ src/main/java/com/example/jee/printing/platform/controller/AgentDashboardServlet.java
@@ -19,14 +19,11 @@ public class AgentDashboardServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Récupérer les tâches de l'agent de tirage depuis la base de données en utilisant PrintTaskDAO
         List<Task> tasks = printTaskDAO.getAgentTasks();
-        // Passer les tâches à la page JSP
         request.setAttribute("tasks", tasks);
         request.getRequestDispatcher("/views/agentDashboard.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Gérer les requêtes POST si nécessaire
     }
 }
